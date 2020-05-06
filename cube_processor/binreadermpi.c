eefc55893036f44d52a89a62b87defad58a15f60
Ilknur <ilknuri607@gmail.com>
2020-05-06 11:07:57 +0300

    Add files via upload

--- cube_processor/binreadermpi.c
+++ cube_processor/binreadermpi.c
@@ -12,10 +12,12 @@ int main(int argc, char *argv[]){
 	int rank,size;
 	int ierr;
 	
+	//generates a default 0 cube if the flag exists
 	if(argc==8){
-			gen(atoi(argv[4]),atoi(argv[5]),atoi(argv[6]));
+		gen(atoi(argv[4]),atoi(argv[5]),atoi(argv[6]));
 	}
 	
+	//MPI Inits
 	MPI_Init(&argc,&argv);
 	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 	MPI_Comm_size(MPI_COMM_WORLD,&size);
@@ -25,6 +27,10 @@ int main(int argc, char *argv[]){
 
 	
 	int r=0,c=0,h=0;
+	
+	/*Master reads the dims from the metadata file and 
+	broadcasts it to the other ranks
+	*/
 	if(rank==0){
 		
 		FILE* fp;
@@ -44,36 +50,34 @@ int main(int argc, char *argv[]){
 	MPI_Bcast(&r,1,MPI_INT,0,MPI_COMM_WORLD);
 	MPI_Bcast(&c,1,MPI_INT,0,MPI_COMM_WORLD);
 	MPI_Bcast(&h,1,MPI_INT,0,MPI_COMM_WORLD);
-	//printf("proc: %d r: %d, c: %d\n",rank,r,c);
 	
-	
+	//Take endpoints from argv
 	int x0=atoi(argv[1]),y0=atoi(argv[2]),z0=atoi(argv[3]);
 	x0--;y0--;z0--;
 	int x1=atoi(argv[4]),y1=atoi(argv[5]),z1=atoi(argv[6]);
 	int x,y,z;
 	int offset;
 	
+	//Init input and output files
 	ierr = MPI_File_open(MPI_COMM_WORLD,"in.bin",
 	MPI_MODE_RDONLY,MPI_INFO_NULL,&fpIn);
 	ierr = MPI_File_open(MPI_COMM_WORLD,"out.bin",
 	MPI_MODE_WRONLY,MPI_INFO_NULL,&fpOut);
+	
+	/*Each rank takes slices that are increments of
+	the num of ranks
+	*/
 	for(z=z0+rank;z<z1;z=z+size){
-		printf("rank %d\n",rank);
 		for(y=y0;y<y1;y++){
 			for(x=x0;x<x1;x++){
-				offset = sizeof(int)*((c)*y+x+
-				(z*r*(c)));
+				offset = sizeof(int)*(c*y+x+
+				z*r*c);
 				
 				MPI_File_read_at(fpIn,offset,&num,
 				1,MPI_INT,MPI_STATUS_IGNORE);
-				//fseek(fp,offset,SEEK_SET);
-				//fread(&num,sizeof(int),1,fp);
-				//printf("%d ",num);
 				
 				num = proc(num);
-				//printf("mpi %d ",num);
-				//fseek(fp,offset,SEEK_SET);
-				//fwrite(&num,sizeof(int),1,fp);
+				
 				MPI_File_write_at(fpOut,offset,&num,
 				1,MPI_INT,status);
 				
@@ -83,12 +87,11 @@ int main(int argc, char *argv[]){
 	}
 	
 	
-	//printmat(fpIn);
-	
-	//MPI_Wait(request,status);
+	//Close files
 	MPI_File_close(&fpIn);
 	MPI_File_close(&fpOut);
-		
+	
+	//Output the new matrix with the master rank
 	if(rank==0){
 		FILE *f;
 		f = fopen("out.bin","rb");
@@ -98,7 +101,6 @@ int main(int argc, char *argv[]){
 	}
 
 	MPI_Finalize();
-	
 	
 	
 }
