fb6d27e73988db2b9a56514d6ee6773b82d49da6
Ilknur <ilknuri607@gmail.com>
2020-07-28 12:52:56 +0400

    Add files via upload

--- Hydra_MLFlow_Test/runner.py
+++ Hydra_MLFlow_Test/runner.py
@@ -15,16 +15,18 @@ def run(cfg : DictConfig):
     z0 = cfg['coordinates']['z0']
     z1 = cfg['coordinates']['z1']
     numProcs = cfg['ranks']
+    numThreads = 4//numProcs
     gen = cfg['gen']
+    threadingLayer = cfg['threading']
     
     print('Running...')
-    print('mpiexec -n {} main_hydra.py {} {} {} {} {} {} {}'
-          .format(numProcs,x0,y0,z0,x1,y1,z1,gen))
+    print('NUMBA_THREADING_LAYER={} NUMBA_NUM_THREADS={} mpiexec -n {} main_hydra.py {} {} {} {} {} {} {}'
+          .format(threadingLayer,numThreads,numProcs,x0,y0,z0,x1,y1,z1,gen))
     
-    filepath = '/home/ilknull/Files/Code/Python/Proc_wHydraMLFlow/'
+    filepath = '/home/ilknull/Files/Code/HPC-Study-master/Hydra_MLFlow_Test/'
 
-    os.system('mpiexec -n {} {}main_hydra.py {} {} {} {} {} {} {}'
-              .format(numProcs,filepath,x0,y0,z0,x1,y1,z1,gen))
+    os.system('NUMBA_THREADING_LAYER={} NUMBA_NUM_THREADS={} mpiexec -n {} python3 {}main_hydra.py {} {} {} {} {} {} {}'
+              .format(threadingLayer,numThreads,numProcs,filepath,x0,y0,z0,x1,y1,z1,gen))
     
 if __name__ == "__main__":
     run()
